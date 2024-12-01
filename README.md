25263f7d39d4e5ded9d64765eefd8f6a3f861a5b
Matas <98917854+shrub2@users.noreply.github.com>
2024-12-01 17:49:10 +0200

    Tree data structure

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Shows a tree of directories.
 
 To-do list:
 - [ ] Implement scrolling
+<<<<<<< HEAD
 - [ ] Implement tree structure for storing path directories
+=======
+- [x] Implement tree structure for storing path directories
+- [ ] Implement function for tree traversal
+>>>>>>> b743a7c (Tree data structure)
--- file-tree.py
+++ file-tree.py
@@ -2,30 +2,51 @@ import curses
 from curses import wrapper
 import os
 
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.children = []
+    
+    def add_child(self, child):
+        self.children.append(child)
+
+def FileTree(path):
+
+    file_tree = Node(path) 
+    
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                file_tree.add_child(FileTree(entry.path))
+    
+    return(file_tree)
+
+
 # Current Working Directory
 cwd = os.getcwd()
 
-text_lines = []
+file_tree = FileTree(cwd)
 
-for root, dirs, files in os.walk(cwd):
-    for dir in dirs:
-        text_lines.append(os.path.join(root, dir))
+#text_lines = []
+#for root, dirs, files in os.walk(cwd):
+    #for dir in dirs:
+        #text_lines.append(os.path.join(root, dir))
 
-text = "\n".join(text_lines)
-
-def main(stdscr):
-    stdscr.scrollok(True)
-    stdscr.clear()
-
-    stdscr.addstr(text)
-
-    stdscr.refresh()
-    while(True):
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
-
-wrapper(main)
+#text = "\n".join(text_lines)
+#
+#def main(stdscr):
+#    stdscr.scrollok(True)
+#    stdscr.clear()
+#
+#    stdscr.addstr(text)
+#
+#    stdscr.refresh()
+#    while(True):
+#        key = stdscr.getch()
+#        if key == ord('q'):
+#            break
+#
+#wrapper(main)
 
 
 
