0221332830ee438caa3e90b727b2f1e51d1f7abe
Matas <98917854+shrub2@users.noreply.github.com>
2024-12-02 09:08:27 +0200

    Update file-tree.py
    Added backward reference for nodes

--- file-tree.py
+++ file-tree.py
@@ -6,8 +6,10 @@ class Node:
     def __init__(self, data = None):
         self.data = data
         self.children = []
+        self.parent = None
     
     def add_child(self, child):
+        child.parent = self
         self.children.append(child)
 
 def FileTree(path):
