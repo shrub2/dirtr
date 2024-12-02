2a5e3c108adf52bda38f929bb735c8b694f336f4
Matas <98917854+shrub2@users.noreply.github.com>
2024-12-02 19:27:00 +0200

    more functions

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ Shows a tree of directories.
 To-do list:
 - [ ] Implement scrolling
 - [x] Implement tree structure for storing path directories
-- [ ] Implement function for tree traversal
--- file-tree.py
+++ file-tree.py
@@ -12,6 +12,45 @@ class Node:
         child.parent = self
         self.children.append(child)
 
+    def get_index_as_child(self):
+        if self.parent is None:
+            return None
+
+        for index, child in enumerate(self.parent.children):
+            if child is self:
+                return index
+
+        return None
+
+    def get_sibling(self, offset):
+        index = self.get_index_as_child()
+
+        if index is not None and 0 <= index + offset < len(self.parent.children):
+            return self.parent.children[index + offset]
+
+        return None
+    
+    def get_left_sibling(self):
+        return self.get_sibling(-1)
+    
+    def get_right_sibling(self):
+        return self.get_sibling(1)
+
+    def get_middle_child(self):
+        n = len(self.children)
+        
+        if n == 0:
+            return None
+        
+        if n % 2 == 1:
+            return self.children[n // 2]
+        else:
+            return self.children[n // 2 - 1]
+
+
+# class Node end
+
+
 def FileTree(path):
 
     file_tree = Node(path) 
@@ -29,13 +68,32 @@ cwd = os.getcwd()
 
 file_tree = FileTree(cwd)
 
+
+### Debugging ###
+
+#index = 3
+#
+#print(file_tree.children[0].children[index].get_left_sibling().data)
+#print(file_tree.children[0].children[index].data)
+#print(file_tree.children[0].children[index].get_right_sibling().data)
+#print("\n")
+#for child in file_tree.children[0].children:
+#    print(child.data)
+#
+#print(file_tree.children[0].get_middle_child().data)
+
+###########################
+
+
 #text_lines = []
 #for root, dirs, files in os.walk(cwd):
     #for dir in dirs:
         #text_lines.append(os.path.join(root, dir))
 
 #text = "\n".join(text_lines)
-#
+
+
+# Curses
 #def main(stdscr):
 #    stdscr.scrollok(True)
 #    stdscr.clear()
